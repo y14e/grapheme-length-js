8f02c027b358d99d51537e2c617abc4e041671c3
Yusuke Kamiyamane <y@y14e.com>
2025-06-14 20:55:45 +0900

    20250614

--- grapheme-length.js
+++ grapheme-length.js
@@ -1,3 +1,3 @@
 export function getGraphemeLength(string) {
-  return [...new Intl.Segmenter(document.documentElement.getAttribute('lang') || 'en').segment(string)].length;
+  return [...new Intl.Segmenter(document.documentElement.lang || 'en').segment(string)].length;
 }
--- grapheme-length.ts
+++ grapheme-length.ts
@@ -1,3 +1,3 @@
 export function getGraphemeLength(string: string): number {
-  return [...new Intl.Segmenter(document.documentElement.getAttribute('lang') || 'en').segment(string)].length;
+  return [...new Intl.Segmenter(document.documentElement.lang || 'en').segment(string)].length;
 }
