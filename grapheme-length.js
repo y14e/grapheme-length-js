28cc67e92f082534331823394db7ad98eb8fdace
Yusuke Kamiyamane <y@y14e.com>
2026-04-11 18:12:28 +0900

    20260411

--- grapheme-length.js
+++ grapheme-length.js
@@ -1,9 +1,6 @@
 const segmenter = new Intl.Segmenter();
 
 export function getGraphemeLength(string) {
-  if (typeof string !== 'string') {
-    return 0;
-  }
   let count = 0;
   for (const _ of segmenter.segment(string)) {
     count++;
