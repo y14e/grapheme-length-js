3636686582dbfa4179ae45c96f182d9dcfc571e1
Yusuke Kamiyamane <y@y14e.com>
2026-04-11 21:54:11 +0900

    Delete grapheme-length.js

--- grapheme-length.js
+++
@@ -1,9 +0,0 @@
-const segmenter = new Intl.Segmenter();
-
-export function getGraphemeLength(string) {
-  let count = 0;
-  for (const _ of segmenter.segment(string)) {
-    count++;
-  }
-  return count;
-}
