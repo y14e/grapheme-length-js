2cde4d4cbb2c471b72a0fdd692b05088978c583c
Yusuke Kamiyamane <y@y14e.com>
2026-04-17 18:53:35 +0900

    Update grapheme-length.ts

--- grapheme-length.ts
+++ grapheme-length.ts
@@ -2,8 +2,10 @@ const segmenter = new Intl.Segmenter();
 
 export function getGraphemeLength(string: string): number {
   let count = 0;
+
   for (const _ of segmenter.segment(string)) {
     count++;
   }
+
   return count;
 }
